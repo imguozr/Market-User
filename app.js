811ebbd5918a0b49590af8db996e51338daaa461
imguozr <imguozr@gmail.com>
2019-12-03 19:40:27 -0600

    Logger to file.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const Koa = require('koa');
+const fs = require('fs');
+const stripAnsi = require('strip-ansi');
 const router = require('koa-router')();
 const koaLogger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
@@ -11,14 +13,19 @@ const stock = require('./server/routers/stock');
 const stockExchange = require('./server/routers/stockExchange');
 
 const app = new Koa();
+const logFile = fs.createWriteStream('log.txt', { flags: 'a' });
+const logStdout = process.stdout;
 
-app.use(koaLogger());
+app.use(koaLogger((str, args) => {
+    logFile.write(stripAnsi(str) + '\n');
+    logStdout.write(str + '\n');
+}));
 
 app.use(bodyParser());
 
 app.use(cors());
 
-router.use('/test', index.routes(), index.allowedMethods());
+// router.use('/test', index.routes(), index.allowedMethods());
 router.use('/user', user.routes(), user.allowedMethods());
 router.use('/auth', github.routes(), github.allowedMethods());
 router.use('/stock', stock.routes(), stock.allowedMethods());
