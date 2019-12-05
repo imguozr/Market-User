3c1d2807749e6efca6af04e4909145138396bd28
imguozr <imguozr@gmail.com>
2019-12-04 22:11:21 -0600

    Deploy on heroku

--- config/database.config.js
+++ config/database.config.js
@@ -4,5 +4,6 @@ module.exports = {
     USERNAME: 'silvermont',
     PASSWORD: 'silvermont',
     PORT: '3306',
-    HOST: 'localhost'
+    HOST: 'localhost',
+    CLEARDB_DATABASE_URL: 'mysql://bcb3d230f9fb89:3c19d169@us-cdbr-iron-east-05.cleardb.net/heroku_2c56ef7eacb8452?reconnect=true'
 };
